73192b3a7579a33f27e59c6b7bfaf7a97031665b
DongHyeok Kim <briankim0910@gmail.com>
2024-01-04 22:58:09 +0900

    commit

--- src/day3/.idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,5 +0,0 @@
-
-8
-day3.iml,b/d/bd92dd12d8b564e723e674c885de2aeb6426b729
-<
-Ex02Opr.java,a/3/a3f47b3f4b3d5a2d0a26429397d9cfa1e9286bce
---
+++ test/src/day01/Ex01.java
@@ -0,0 +1,15 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        Scanner sc = new Scanner(System.in);
+        int num;
+        num = sc.nextInt();
+
+        System.out.println(num);
+    }
+}
