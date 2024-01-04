4be717752567bd531805884454d8ec240d440e99
DongHyeok Kim <briankim0910@gmail.com>
2024-01-04 21:34:08 +0900

    commit

---
+++ day3/Ex04Opr.java
@@ -0,0 +1,33 @@
+package day3;
+
+public class Ex04Opr {
+
+	public static void main(String[] args) {
+
+		// 5.논리 연산자
+		// ! (NOT)
+		// && (AND) 
+		// || (OR)
+		
+		// 1) ! (Not)
+		// boolean 결과를 반대로 뒤집는다
+		
+		System.out.println(!true);
+		System.out.println(!false);
+		
+		boolean isRainy = true;
+		
+		isRainy = !isRainy;
+		
+		// 2) && (AND)
+		// (피연산자) && (피연산자)
+		
+		//true && true
+		//true && false
+		//false && true
+		//false && false
+		
+		
+	}
+
+}
