c3989bc31fac24b0aa693bb94a6b9c5ed556c409
DongHyeok Kim <briankim0910@gmail.com>
2024-01-18 11:43:00 +0900

    Update Ex02.java

--- test/src/day12/Ex02.java
+++ test/src/day12/Ex02.java
@@ -22,7 +22,26 @@ public class Ex02 {
 			System.out.print(team.get(i) + " "); 
 		}
 		System.out.print("입니다");
-
+		
+		// 팀원 퇴출시키기
+		// 1. 퇴출할 팀원 이름 입력
+		// 2. team ArrayList의 0 ~ 마지막 인덱스를 검사한 후
+		//내가 입력한 팀원이 있으면 삭제한후 다시 팀원 출력
+		System.out.println();
+		System.out.print("퇴출시킬 팀원 : ");
+		String getOut = sc.next();
+	
+		for(int i = 0; i < team.size(); i++) {
+			if(team.get(i).equals(getOut)) {
+		team.remove(getOut);
+		break;
+			}
+		}
+		System.out.print("김동혁의 팀원은 ");
+		for (int i = 0; i < team.size(); i++) {
+			System.out.print(team.get(i) + " "); 
+		} 
+		System.out.print("입니다");	
 	}
 
 }
